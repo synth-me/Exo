0c41b64e2b1b3e7af6fa192f8f2bc53d259510aa
Muriel Panegassi <murielpanegass1@gmail.com>
2022-03-04 16:51:12 -0300

    added the build perl file
    If you want to develop , if you can download the source code and compile it all with this script

---
+++ compile.pl
@@ -0,0 +1,34 @@
+use warnings ; 
+use strict ; 
+
+sub insert_variables {
+  # here we insert the dependencies and the path of the variables 
+};
+
+sub install_dependencies {
+
+  # here we download the dependencies 
+
+  my $python_dependencies = "";
+  my $stack_dependencies  = "";
+  my $perl_dependencies   = "";
+  my $node_dependencies   = ""; 
+};
+
+sub check_dependencies {
+  # here we check the after installation 
+};
+
+sub compile_source_codes {
+  # here we compile the codes to single executables including python , perl and javascript scripts 
+};
+
+sub main {
+  print "Started compilation ... \n" ; 
+  install_dependencies();
+  check_dependencies();
+  compile_source_codes();
+  insert_variables();
+};
+
+main();
