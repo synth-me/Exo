c9a2465565022abc11c80bed72d46cccd911a6f1
Muriel Panegassi <murielpanegass1@gmail.com>
2022-03-08 11:36:01 -0300

    Update the compile file
    Added the compilation of each haskell's file and C file and the vendorizatin of network mqtt

--- compile.pl
+++ compile.pl
@@ -1,26 +1,94 @@
-use warnings ; 
 use strict ; 
+use HTTP::Request;
+use LWP::UserAgent ();  
+use Data::Dumper; 
+use Cwd qw(getcwd);
 
 sub download_mqtt_package {
-  # here we download the mqtt basic package and vendorize it in everry folder of the
-  # project 
+	my $arbritary_source = "Arbitrary.hs";
+	my $client_source    = "Client.hs";
+	my $topic_source     = "Topic.hs";
+	my $types_sources    = "Types.hs";
+	my @l = ($arbritary_source,$client_source,$topic_source,$types_sources);		
+	my $ua = LWP::UserAgent->new(timeout => 10);
+	$ua->env_proxy;
+	my %hash_file_content = (); 
+	print "Collecting shared library ... \n"; 
+	foreach my $file (@l){
+		my $template = "https://raw.githubusercontent.com/dustin/mqtt-hs/master/src/Network/MQTT/$file";
+		my $response = $ua->get($template);
+		$hash_file_content{$file} = $response->decoded_content;
+	};
+	my @folders = ("aggregator","plugins","exo-cli","exo-server");
+	my $path = "";
+	foreach my $folder (@folders){
+		chdir("./$folder");
+		$path = getcwd() ;
+		print "Vendorizing on :: $path \n" ;
+		mkdir("Network");
+		chdir("./Network");
+		mkdir("MQTT");
+		chdir("./MQTT");	
+		foreach my $key (keys %hash_file_content){
+			open(FH,">",$key);
+			print FH $hash_file_content{$key} ;
+			print "Wrote $key \n";
+			close(FH);
+		};
+		chdir("..");
+		chdir("..");
+		chdir("..");
+
+		print getcwd() ; 
+	};
+	return 1 ; 
 };
 
-sub insert_variables {
-  # here we insert the dependencies and the path of the variables 
+
+sub compile_haskell_source {
+	print "Compiling Haskell : \n";
+	my @folders = ("aggregator","plugins","exo-cli","exo-server"); 
+	my $comand_aggr = "stack ghc -- aggregator.hs -threaded";
+	my $command_plg = "stack ghc -- Plugins.hs -threaded";
+	my $command_epm = "stack ghc -- epm.hs -threaded";
+	my $command_ser = "stack ghc -- server.hs -threaded";
+	my @command_list = ($comand_aggr,$command_plg,$command_epm,$command_ser);
+	my $counter = 0 ;
+	my $path = "";
+	while($counter < 4){
+		chdir("./$folders[$counter]");
+		$path = getcwd() ; 
+		print "Running on :: $path the command : $command_list[$counter] \n"; 
+		system $command_list[$counter] ;
+		chdir("..");
+		$counter++ ; 	
+	}; 
+	return 1 ;
 };
 
-sub install_dependencies {
-
-  # here we download the dependencies 
-
-  my $python_dependencies = "";
-  my $stack_dependencies  = "";
-  my $perl_dependencies   = "";
-  my $node_dependencies   = ""; 
+sub compile_C_source{
+	chdir("./puppet") ;
+	my $path = getcwd() ;
+	my $command_C = "gcc -shared -o ./puppet.dll ./puppet.c"; 
+	print "Compiling C dll or SO : \n";
+	print "Running on :: $path the command : $command_C \n";
+	chdir("..");
+	return 1 ;
 };
 
-sub check_dependencies {
+
+sub main{
+	download_mqtt_package();
+	compile_haskell_source();
+	compile_C_source();
+};
+
+
+main();
+
+
+# eof 
+endencies {
   # here we check the after installation 
 };
 
